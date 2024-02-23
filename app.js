a563e8eac33a10a21964a02f1b1667894facde60
SrHebert <ronnyhebert00@gmail.com>
2024-02-23 19:43:55 -0300

    iniciando manipulação JAvascript

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-var entradaTexto = document.getElementsByClassName("caixa-de-texto")
-var textoCoDecodificado = document.getElementsByClassName("mensagem-co-decodificada")
+var btnCriptografar = querySelector('.botao-criptografar');
+var btnDescriptografar = querySelector('.botao-descriptografar');
+var imagemMeninoComLupa = querySelector('.img-menino-com-lupa');
+var paragrafoNadaEncontrado = querySelector('.segunda-caixa-paragrafos');
+var textoResutado = querySelector('.resultado');
 
-let letrasCodficadas = {
-    ["e", "enter"]
-    ["i", 'imes']
-    ["a","ai"]
-    ["o", "ober"]
-    ["u", "ufat"]
-};
+btnCriptografar.onclick = criptografar;
+btnDescriptografar.onclick = descriptografar;
 
-let caixaTexto = document.querySelector('caixa-de-texto');
-caixaTexto.getAttribute("textarea");
-caixaTexto.innerHTML = 'Digite seu texto aqui...';
+let letrasCodificadas = [
+    ["e", "enter"],
+    ["i", 'imes'],
+    ["a","ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+]
+
+function criptografar(string) {
+    caixaDeTexto2 = resultado;
+    caixaDeTexto1 = string
+    const resultado = string
+        .replaceAll("e","enter")
+        .replaceAll("i","imes")
+        .replaceAll("a","ai")
+        .replaceAll("o","ober")
+        .replaceAll("u","ufat");
+
+        return resultado;
+}
+
+function descriptografar(string){
+        const resultado = string
+        .replaceAll("enter","e")
+        .replaceAll("imes","i")
+        .replaceAll("ai","a")
+        .replaceAll("ober","o")
+        .replaceAll("ufat","u");
+
+        return resultado;
+}
+
+function btnCriptografarTexto(){
+    console.log(resultado)
+}
+
+let test = "texto de teste";
+let testDescriptografado = "tenterxtober denter tenterstenter"
+
+console.log(criptografar(test));
+console.log(descriptografar(testDescriptografado));
