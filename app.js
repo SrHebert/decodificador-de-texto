af183583f6f31488aaad88ce78a77246f49d3326
SrHebert <ronnyhebert00@gmail.com>
2024-02-26 15:59:20 -0300

    ajustes no java

--- app.js
+++ app.js
@@ -1,65 +1,72 @@
 var btnCriptografar = querySelector('.botao-criptografar');
 var btnDescriptografar = querySelector('.botao-descriptografar');
-var imagemMeninoComLupa = querySelector('.img-menino-com-lupa');
-var paragrafoNadaEncontrado = querySelector('.segunda-caixa-paragrafos');
+var imagemMeninoComLupa = querySelector('.menino-com-lupa');
+var conteudoParagrafos = querySelector('.caixa-paragrafos');
 var textoResutado = querySelector('.resultado');
 
 btnCriptografarTexto.onclick = criptografar;
 btnDescriptoTexto.onclick = descriptografar;
 
-let letrasCodificadas = [
-    ["e", "enter"],
-    ["i", 'imes'],
-    ["a","ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
-]
-
 function criptografar() {
     ocultarParagrafosCaixa2();
-    var caixaDeTexto1 = recuperarTexto()
-    resultado.textContent = criptografar(caixaDeTexto1)
+    var caixaDeTexto = recuperarTexto()
+    resultado.textContent = criptografarTexto(caixaDeTexto)
 }
 
 function descriptografar(){
     ocultarParagrafosCaixa2()
-    var caixaDetexto1 = recuperarTexto()
-    resultado.textContent = descriptografar(caixaDetexto1);
+    var caixaDetexto = recuperarTexto()
+    resultado.textContent = descriptografarTexto(caixaDetexto);
 }
 
 function recuperarTexto(){
-    var caixaDeTexto1 = document.querySelector('.caixa-de-texto')
-    return caixaDeTexto1.value
+    var caixaDeTexto = document.querySelector('.caixa-de-texto')
+    return caixaDeTexto.value
 }
-function ocultarParagrafosCaixa2(){
+function ocultarParagrafos(){
     imagemMeninoComLupa.classList.add(".ocultar");
-    segunda-caixa-paragrafos.classList.add(".ocultar")
-}
-    const resultado = string
-        .replaceAll("e","enter")
-        .replaceAll("i","imes")
-        .replaceAll("a","ai")
-        .replaceAll("o","ober")
-        .replaceAll("u","ufat");
-
-        return resultado;
+    conteudoParagrafos.classList.add(".ocultar")
 }
 
-function descriptografar(string){
-        const resultado = string
-        .replaceAll("enter","e")
-        .replaceAll("imes","i")
-        .replaceAll("ai","a")
-        .replaceAll("ober","o")
-        .replaceAll("ufat","u");
+function criptografarTexto(mensagem){
+    var texto = mensagem;
+    var textoFinal = "";
 
-        return resultado;
+    for(var i = 0; 0< texto.leng; i++){
+        if(texto[i]== "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i]== "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i]== "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i]== "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i]== "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else {
+            textoFinal = textoFinal = texto[i]
+        }
+        
+    }
+    return textoFinal
 }
 
-function btnCriptografarTexto(){
-    console.log(resultado);
+function copiar(){
+    let copiarTexto = document.getElementsByClassName(".caixa-de-texto");
+
+    copiarTexto.select();
+    document.execCommand("copy");
+    alert("Texto copiado.");
 }
 
-function btnDescriptografar(){
-    console.log("O botão foi clicado!");
-}
+const btnCopiar = document.querySelector(".btn-copiar");
+    btnCopiar.addEventListener ("click", copiar = () => {
+    var conteudo = document.querySelector(".texto-resultado").textContent;
+    navigator.clipboard.writeText(conteudo);
+    console.log("Olá");    
+    })
